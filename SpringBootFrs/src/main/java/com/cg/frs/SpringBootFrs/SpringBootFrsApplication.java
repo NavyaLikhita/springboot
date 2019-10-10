fe2b20da209dec8847a5397484ea9865cfa04143
NavyaLikhita <54634629+NavyaLikhita@users.noreply.github.com>
2019-10-10 07:48:03 +0530

    Add files via upload

---
+++ SpringBootFrs/src/main/java/com/cg/frs/SpringBootFrs/SpringBootFrsApplication.java
@@ -0,0 +1,14 @@
+package com.cg.frs.SpringBootFrs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootFrsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootFrsApplication.class, args);
+		System.out.println("Its up and running");
+	}
+
+}
